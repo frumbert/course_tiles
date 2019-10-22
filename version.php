4ca5f65d3ad6af1a0d5b05cea5585b54002454a7
Tim St.Clair <tim.stclair@gmail.com>
2019-10-22 16:58:43 +1100

    version bump for language pack

--- version.php
+++ version.php
@@ -18,12 +18,12 @@
  * Version details
  *
  * @package    block_course_tiles
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @copyright  tim@avideelearning.com
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018120300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019102200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018112800;        // Requires this Moodle version
 $plugin->component = 'block_course_tiles'; // Full name of the plugin (used for diagnostics)
